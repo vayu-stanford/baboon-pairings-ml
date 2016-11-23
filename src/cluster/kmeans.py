be3245dcced4531c035b64e18aba31f19e5b1bf8
vayu-stanford <vayu@stanford.edu>
2016-11-23 04:29:35 -0500

    print out centroids

--- src/cluster/kmeans.py
+++ src/cluster/kmeans.py
@@ -11,20 +11,21 @@ def apply_kmeans_consort(num_clusters, include_transformed):
     (X,y) = extract.generate_labelled_data(valid_labels=['1'], label_type='consort',include_transformed=include_transformed)
     km = KMeans(n_clusters=num_clusters, random_state=RANDOM_SEED)
     preds = km.fit_predict(X)
-    return (X, preds)
+    return (X, preds, km.cluster_centers_)
 
 def apply_kmeans_nonconsort(num_clusters, include_transformed):
     (X,y) = extract.generate_labelled_data(valid_labels=['0'], label_type='consort',include_transformed=include_transformed)
     km = KMeans(n_clusters=num_clusters, random_state=RANDOM_SEED)
     preds = km.fit_predict(X)
-    return (X, preds)
+    return (X, preds, km.cluster_centers_)
 
 def main():
     include_transformed=True
-    (X, preds) = apply_kmeans_consort(4,include_transformed=include_transformed)
+    (X, preds, cluster_centers) = apply_kmeans_nonconsort(4,include_transformed=include_transformed)
     # PCA is faster, but manifold gives better separation
     # pca_vis.pca_vis_2d(X, preds)
-    manifold_vis.manifold_vis_2d(X, preds)
+    print(cluster_centers)
+    #manifold_vis.manifold_vis_2d(X, preds)
 
 if __name__=="__main__":
     main()
